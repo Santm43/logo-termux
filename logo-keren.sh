8a5ecfe6dee3275f9ae6ddc920520ea78569298d
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-04-30 05:31:00 +0700

    Add files via upload

---
+++ logo-keren.sh
@@ -0,0 +1,7 @@
+#mau rekode ya meq?
+#kencing aja belum lurus
+#acep x-code
+
+z="
+";dz='a';Mz='tch';Pz='inux';Yz='-A A';Gz='ols"';Az='figl';sz='p 1';Zz='ndro';Xz='n';Cz='f sl';cz='edor';fz='int';Oz='-A L';Wz='ebia';nz='gins';qz='s In';bz='-A F';jz=' Kas';Dz='ant ';iz='rima';lz='elah';Ez='"Ace';Bz='et -';Fz='p To';hz=' "Te';pz='Tool';Lz='enfe';Kz='scre';Uz='u';ez='-A M';Sz='-A U';oz='tal ';rz='i"';Iz='inst';gz='echo';az='id';Jz='all ';Rz='p 5';Vz='-A D';Tz='bunt';Qz='slee';Nz='tch ';mz=' Men';kz='ih T';Hz='pkg ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$z$Kz$Lz$Nz$Oz$Pz$z$Qz$Rz$z$Kz$Lz$Nz$Sz$Tz$Uz$z$Qz$Rz$z$Kz$Lz$Nz$Vz$Wz$Xz$z$Qz$Rz$z$Kz$Lz$Nz$Yz$Zz$az$z$Qz$Rz$z$Kz$Lz$Nz$bz$cz$dz$z$Qz$Rz$z$Kz$Lz$Nz$ez$fz$z$Qz$Rz$z$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$z$Qz$sz"
